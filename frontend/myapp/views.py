16bdd6108e936c441385ea142107cff6358a62b9
J-Ajsivinac <3114791110409@ingenieria.usac.edu.gt>
2023-10-20 12:29:38 -0600

    file upload functions are added

--- frontend/myapp/views.py
+++ frontend/myapp/views.py
@@ -8,20 +8,51 @@ import requests
 # Create your views here.
 def index(request):
     if request.method == "GET":
-        exito = request.GET.get("exito")
+        if request.GET.get("reset") is not None:
+            exito = None
+            try:
+                api_url = "http://127.0.0.1:3020/limpiarDatos"
+                response = requests.get(api_url, timeout=100)
+                exito = response.status_code == 200
+                print(exito)
+            except Exception as _:
+                exito = False
+                print(exito, "---")
+            return redirect(f"{reverse('index')}?exito={exito}")
+        exito = request.GET.get("exito", None)
+        exito = exito == "True" if exito is not None else None
         return render(request, "index.html", {"exito": exito})
-    elif request.method == "POST" and request.FILES["archivo"]:
-        exito = None
-        try:
-            archivo = request.FILES["archivo"]
-            contenido = archivo.read()
-            api_url = "http://127.0.0.1:3020/grabarConfiguracion"
-            response = requests.post(api_url, data=contenido, timeout=100)
-            exito = response.status_code == 200
-        except Exception as _:
-            exito = False
+    elif request.method == "POST":
+        # cargar diccionario de  configuraciones
+        if "config" in request.FILES:
+            exito = None
+            print("entro en config")
+            try:
+                archivo = request.FILES["config"]
+                contenido = archivo.read()
+                api_url = "http://127.0.0.1:3020/grabarConfiguracion"
+                response = requests.post(api_url, data=contenido, timeout=100)
+                exito = response.status_code == 200
+            except Exception as _:
+                exito = False
 
-        # request.method = "GET"
-        return redirect(f"{reverse('index')}?exito={exito}")
-    exito = request.GET.get("exito")
+            # request.method = "GET"
+            return redirect(f"{reverse('index')}?exito={exito}")
+        # cargar mensajes para analizar
+        elif "msgs" in request.FILES:
+            exito = None
+            print("entro en config")
+            try:
+                archivo = request.FILES["msgs"]
+                contenido = archivo.read()
+                api_url = "http://127.0.0.1:3020/grabarMensajes"
+                response = requests.post(api_url, data=contenido, timeout=1000)
+                exito = response.status_code == 200
+            except Exception as _:
+                exito = False
+
+            return redirect(f"{reverse('index')}?exito={exito}")
+        # return redirect(f"{reverse('index')}?exito={exito}")
+    exito = request.GET.get("exito", None)
+    exito = exito == "True" if exito is not None else None
     return render(request, "index.html", {"exito": exito})
