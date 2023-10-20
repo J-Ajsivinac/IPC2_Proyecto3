93ff8bfcff1490fb5fc67fdc56a91533d133de76
J-Ajsivinac <3114791110409@ingenieria.usac.edu.gt>
2023-10-20 11:17:36 -0600

    the way of sending information is changed

--- frontend/myapp/urls.py
+++ frontend/myapp/urls.py
@@ -2,7 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index),
-    path("/", views.index),
-    path("cargar-archivo/", views.process_configure, name="process_configure"),
+    path("", views.index, name="index"),
 ]
--- frontend/myapp/views.py
+++ frontend/myapp/views.py
@@ -1,27 +1,27 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views.decorators.http import require_POST
+from django.urls import reverse
 import requests
 
 
 # Create your views here.
 def index(request):
-    exito = request.GET.get("exito", None)
-    # Convierte exito en un valor booleano
-    exito = exito == "True" if exito is not None else None
+    if request.method == "GET":
+        exito = request.GET.get("exito")
+        return render(request, "index.html", {"exito": exito})
+    elif request.method == "POST" and request.FILES["archivo"]:
+        exito = None
+        try:
+            archivo = request.FILES["archivo"]
+            contenido = archivo.read()
+            api_url = "http://127.0.0.1:3020/grabarConfiguracion"
+            response = requests.post(api_url, data=contenido, timeout=100)
+            exito = response.status_code == 200
+        except Exception as _:
+            exito = False
+
+        # request.method = "GET"
+        return redirect(f"{reverse('index')}?exito={exito}")
+    exito = request.GET.get("exito")
     return render(request, "index.html", {"exito": exito})
-
-
-def process_configure(request):
-    exito = None
-    if request.method == "POST" and request.FILES["archivo"]:
-        archivo = request.FILES["archivo"]
-        contenido = archivo.read()
-        # print(contenido)
-        api_url = "http://127.0.0.1:3020/grabarConfiguracion"
-        response = requests.post(api_url, data=contenido)
-        exito = response.status_code == 200
-        # request.session["exito"] = exito
-        return redirect(f"/?exito={exito}")
-    # exito = request.session.get("exito", None)
-    return redirect(f"/?exito={exito}")
